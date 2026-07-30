2641f4577c55535f940653bbd04087766028cd3e
Sandip Dahal <er.sandipdahal@gmail.com>
2026-07-30 17:33:58 +0545

    Add manual Docker publish workflow

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ dotnet run train-model.cs
 dotnet run generate-data.cs
 
 dotnet publish -c Release -o Publish ./LoanShark.Ui/LoanShark.Ui.csproj
-dotnet publish -c Release -o ./Publish ./LoanShark.API/LoanShark.API.csproj
+dotnet publish -c Release -o ./Publish ./LoanShark.Api/LoanShark.Api.csproj
