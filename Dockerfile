631299723115be0df1dcd76db29a85238588798a
Sandip Dahal <er.sandipdahal@gmail.com>
2026-07-30 18:40:39 +0545

    fix(ci): fix typo in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM mcr.microsoft.com/dotnet/aspnet:10.0
 WORKDIR /app
 COPY Publish/ .
 
-ENTRYPOINT ["dotnet", "LoanShark.API.dll"]
+ENTRYPOINT ["dotnet", "LoanShark.Api.dll"]
--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ dotnet workload install wasm-tools
 dotnet run train-model.cs
 dotnet run generate-data.cs
 
-dotnet publish -c Release -o Publish ./LoanShark.Ui/LoanShark.Ui.csproj
+dotnet publish -c Release -o ./Publish ./LoanShark.Ui/LoanShark.Ui.csproj
 dotnet publish -c Release -o ./Publish ./LoanShark.Api/LoanShark.Api.csproj
